71e70717e53819ad07267c8f15be8b3b00cf8347
esdeath11 <esdeath11@users.noreply.github.com>
2020-06-07 05:22:02 +0700

    login

--- main.dart
+++ main.dart
@@ -111,7 +111,77 @@ class _Login extends State<Login>{
                         child: Text("Stay Safe, Stay Healthy", style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),),
                       )
                   ),
+                  Positioned(
+                    top: 410,
+                      left: 53,
+                      child: Text("APLIKASI KESEHATAN",style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),)
+                  ),
+                  Positioned(
+                    top: 460,
+                    left: 20,
+                    child: Text("Username:",style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
+                  ),
+                  Positioned(
+                    top: 490,
+                    left: 20,
+                    width: 302,
+                      height: 36,
+                      child:
+                      TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(45)
+                            )
+                        ),
+                      ),
+                  ),
+                  Positioned(
+                      top: 550,
+                      left: 20,
+                      child: Text("Password", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),)),
+                  Positioned(
+                      top: 580,
+                      width: 302,
+                      height: 36,
+                      left: 20,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(45)
+                            )
+                        ),
+                      ),),
+                  Positioned(
+                    width: 95,
+                      height: 38,
+                      left: 220,
+                      top: 650,
+                      child: RaisedButton(
+                      onPressed: (){
 
+                      },
+                        color: Color.fromRGBO(227, 113, 119, 1) ,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(45)
+                      ),
+                      child: Text("LOGIN",style: TextStyle(color: Colors.white),),
+                  )),
+                  Positioned(
+                      top: 750,
+                      left: 33,
+                      child: Row(
+                    children: <Widget>[
+                      Text("Don't Have Account?"),
+                      InkWell(
+                        child: Text(" Sign Up", style: TextStyle(color: Colors.red),),
+                        onTap: (){
+                          Navigator.pushNamed(context, "/second");
+                        },
+
+                      )
+                    ],
+                  ))
                 ],
               ),
             )
